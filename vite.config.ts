50effebc36ac5c60b9dfc827227b3ce58a237df5
nmanumr <nmanumr@gmail.com>
2024-05-28 01:12:23 +0500

    Add GitHub Actions workflow for deploying to GitHub Pages
    This commits a new GitHub Actions workflow that will deploy updates to the GitHub Pages site whenever changes are pushed to the main branch. Additionally, a new environment variable is introduced to the vite config file to properly set the base URL during the build process when deploying via GitHub Actions.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS_BASE ?? '/',
   plugins: [
     svgr({
       svgrOptions: {
